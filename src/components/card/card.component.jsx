a23f7a8bab70b2c89576cbb9c439e36238c3a4bc
Shubham Kumar <sk246903@gmail.com>
2021-08-10 18:50:01 +0530

    Type 2 tooltip

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,22 +1,90 @@
 import React from 'react'
+import styled from 'styled-components';
 import './card.css';
+
 
 const Card = (props) => {
     console.log(props)
-    const {borderRadius=false, size='lg', name, profile, workedAt} = props 
+    const {borderRadius=false, size='lg', name, profile, workedAt, mainBg, profileImg, skillImg} = props 
     return (
-        <div className={`main-wrapper ${size} ${borderRadius?'border-radius':''}`}>
-            <div className="profile-image">
+        <StyledComponents.MainWrapper borderRadius={borderRadius} size={size} skillImg={skillImg} profileImg={profileImg} mainBg={mainBg}>
+            <div className="profile-image" data-tooltip={name}>
                 <div className="skill-image"/>
-                <span className="tooltiptext">
-                    <p>{name}</p>
-                    <p>{profile}</p>
-                    <p>Previously @{workedAt}</p>
-            </span>
             </div>
-            
-        </div>
+        </StyledComponents.MainWrapper>
     )
 }
 
 export default Card
+
+const handleSizeType = (size) => {
+    switch (size) {
+      case "lg":
+        return "width: 100%; height: 100vh";
+      case "md":
+        return "width: 50%; height: 75vh";
+      default:
+        return "width: 25%; height: 50vh";
+    }
+  };
+
+const StyledComponents = {
+    MainWrapper: styled.div`
+        display: flex;
+        background-color: red;
+        justify-content: center;
+        border-radius: ${(props)=>props.borderRadius?'15px':0};
+        align-items: center;
+        background-image: ${(props)=> `url(${props.mainBg})`};
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        ${({ size}) => handleSizeType(size)};
+        .profile-image {
+            background-image: ${(props)=> `url(${props.profileImg})`};
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: contain;
+            flex-direction: column;
+            height: 100px;
+            width: 100px;
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            border-bottom-left-radius: 25px;
+            }
+
+            .skill-image {
+            background-image: ${(props)=> `url(${props.skillImg})`};
+            height: 30px;
+            width: 30px;
+            align-self: flex-start;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: contain;
+            }
+
+            .profile-image::before,
+            .profile-image::after {
+            --scale: 0;
+            content: "";
+            position: absolute;
+            transform: translateY(40px) scale(var(--scale));
+            transition: 150ms transform;
+            }
+
+            .profile-image::before {
+            content: attr(data-tooltip);
+            padding: 0.5rem;
+            background-color: black;
+            color: white;
+            width: max-content;
+            }
+
+            .profile-image:hover::before,
+            .profile-image:hover::after {
+            --scale: 1;
+            }
+    `
+}
+
--- src/components/card/card.stories.js
+++ src/components/card/card.stories.js
@@ -31,5 +31,8 @@ UserCard.args = {
   size:'md',
   name: 'Shubham',
   profile: 'React Dev',
-  workedAt: 'Airbnb'
+  workedAt: 'Airbnb',
+  mainBg: 'https://images.unsplash.com/photo-1581456495146-65a71b2c8e52?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80', 
+  profileImg: 'https://images.unsplash.com/photo-1461800919507-79b16743b257?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1', 
+  skillImg: 'https://miro.medium.com/max/1838/1*6ahbWjp_g9hqhaTDSJOL1Q.png',
 };
